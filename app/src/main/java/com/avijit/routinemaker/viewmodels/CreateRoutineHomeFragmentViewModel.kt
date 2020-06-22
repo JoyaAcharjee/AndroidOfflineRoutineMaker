2b471a108b61a7477b4374738d744d1495141341
Avijit Acharjee <avijitach@gmail.com>
2020-06-23 01:28:05 +0600

    CreateRoutineHomeFragmentViewModel class created

---
+++ app/src/main/java/com/avijit/routinemaker/viewmodels/CreateRoutineHomeFragmentViewModel.kt
@@ -0,0 +1,11 @@
+package com.avijit.routinemaker.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Avijit Acharjee on 6/23/2020 at 1:26 AM.
+ * Email: avijitach@gmail.com.
+ */
+class CreateRoutineHomeFragmentViewModel : ViewModel() {
+
+}
